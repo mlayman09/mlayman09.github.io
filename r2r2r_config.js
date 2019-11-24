7dce281aa02337088b598d2b5d12038db7016f21
mlayman09 <mlayman09@gmail.com>
2019-11-24 13:35:03 -0700

    updates
    final ones

--- r2r2r_config.js
+++ r2r2r_config.js
@@ -7,15 +7,15 @@ var config = {
     title: 'R2R2R',
     subtitle: 'A journey across the Grand Canyon and back in a day',
     byline: 'Oct 11 2019',
-    footer: 'Source: source citations, etc.',
+    footer: 'Built with Mapbox',
     chapters: [
         {
             id: 'slug-style-id',
-            title: 'Display Title',
+            title: 'Preparation',
             image: 'r2r2r_images/Sunrise.jpg',
-            description: 'Sunrise, about 4 miles in',
+            description: 'Around two years prior, my friend Vincent and I were out running some Virginia country roads, and came up with the idea of running across the Grand Canyon and back in a day, commonly known as the Rim to Rim to Rim run or R2R2R. Fall of 2019 became our target, and we spent most of 2019 preparing for this day. With a favorable weather forecast for the day, and months of training logged, we were ready to embark upon one of the longest runs of our lives.',
             location: {
-                center: [-112.56091, 36.40448],
+                center: [-112.56091, 36.33000],
                 zoom: 9,
                 pitch: 0,
                 bearing: 0
@@ -35,9 +35,9 @@ var config = {
         },
         {
             id: '2',
-            title: 'The beginning of our journey- The South Kaibab Trailhead',
+            title: 'The beginning of our journey- descending to the Colorado River',
             image: 'r2r2r_images/River.jpg',
-            description: 'Description here',
+            description: 'Pitch black. That\'s what I remember about the first mile or two of our run. But gradually, as the sun angled it\'s way toward us, we began to see the canyon walls. Ambling down the trail we made sure to take plenty of photos and save our quads for later. 7 miles later we arrived at the Colorado River.',
             location: {
                 center: [-112.08770, 36.05800],
                 zoom: 14,
@@ -52,7 +52,7 @@ var config = {
             id: '3',
             title: 'Start of the North Rim ascent',
             image: 'r2r2r_images/InnerCanyon.jpg',
-            description: 'Description here',
+            description: 'After filling our water bottles with NASCAR pit crew efficiency at Phantom Ranch, we began our climb of the North Rim with controlled earnest. Slight incline in the trail? Hike it. We were sticking to our game plan of staying as fresh as possible for as long as possible. The intimacy of the box canyon slowly gave way to a wider, drier canyon floor. 4 hours in we finally felt the sun on our skin.',
             location: {
                 center: [-112.08900, 36.11806],
                 zoom: 14,
@@ -65,9 +65,9 @@ var config = {
 		
 		{
             id: '4',
-            title: 'Up up up!',
+            title: 'It\'s right there!',
             image: 'r2r2r_images/NorthRimAscent.jpg',
-            description: 'Description here',
+            description: 'Once we passed Manzanita, the true climb began. Hanging gardens and precipitous drop offs kept us mildly distracted from the task at hand. A lovely aspen grove and expansive views southwards towards the San Francisco peaks signaled our proximity to the halfway point. Surely, we thought, after THIS switchback the North Rim trailhead will appear.',
             location: {
                 center: [-112.04437, 36.17999],
                 zoom: 14,
@@ -80,9 +80,9 @@ var config = {
 		
 		{
             id: '5',
-            title: 'North Rim! Now...we just have to do that again!',
+            title: 'The North Rim. Now...we just have to do that again!',
             image: 'r2r2r_images/NorthRim.jpg',
-            description: 'Description here',
+            description: 'About 6 hours in, we reached the North Rim. We refueled and sat down for around 10 minutes or so, then began the long descent back down to the river. Around 24 miles or so to go, and we were feeling quite alright.',
             location: {
                 center: [-112.08101, 36.20500],
                 zoom: 13,
@@ -97,7 +97,7 @@ var config = {
             id: '6',
             title: 'Back to the River',
             image: 'r2r2r_images/NorthRimViews.jpg',
-            description: 'Description here',
+            description: 'Now to retrace our steps back to Phantom Ranch. Thankfully the chill of the North Rim rapidly disappeared as we went down. Around Manzanita we passed some day hikers we had seen much earlier in the day on the South Kaibab Trail. We briefly thought about hiking over to see Ribbon Falls, but decided to leave that for another day.',
             location: {
                 center: [-112.04437, 36.17999],
                 zoom: 14,
@@ -112,9 +112,9 @@ var config = {
             id: '7',
             title: 'One final push to go',
             image: 'r2r2r_images/Afternoon.jpg',
-            description: 'Description here',
+            description: 'Soon enough we arrived back at Phantom Ranch. The final climb that was in the back of our minds all day was here, and there was nothing to do but go up! After filling our bottles we crossed the river once again as evening began in the canyon.',
             location: {
-                center: [-112.08900, 36.11806],
+                center: [-112.105000, 36.100567],
                 zoom: 14,
                 pitch: 20,
                 bearing: 15
@@ -127,7 +127,7 @@ var config = {
             id: '8',
             title: 'Sunset in the canyon',
             image: 'r2r2r_images/Sunset.jpg',
-            description: 'Description here',
+            description: 'An eerie yet peaceful quiet had come over the canyon. The near constant encounters with other hikers and runners had all but stopped. The glances at the watch and mental calculations of mileage remaining became more frequent. And yet, in the midst of the hardest part of the day, we experienced some truly beautiful moments. Notably, a cloudless sunset followed by a nearly full moon illuminating the path ahead.',
             location: {
                 center: [-112.139999, 36.07965],
                 zoom: 14,
@@ -142,10 +142,10 @@ var config = {
             id: '9',
             title: 'Finished, in more ways than one',
             image: 'r2r2r_images/Finish.jpg',
-            description: 'Description here',
+            description: 'Around 7pm, nearly 14 hours after we had started, and 44 miles and 10,500\' of elevation gain later, we were done for the day. A day well spent, and not easily forgotten!',
             location: {
                 center: [-112.14, 36.12],
-                zoom: 12,
+                zoom: 11,
                 pitch: 30,
                 bearing: 0
             },
